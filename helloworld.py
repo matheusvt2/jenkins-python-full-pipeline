afe7ad27b8b4ba26a913952d5e2f515f9a250346
matheusvt2 <41554150+matheusvt2@users.noreply.github.com>
2019-10-06 14:08:25 -0300

    Create helloworld.py

---
+++ helloworld.py
@@ -0,0 +1,31 @@
+#! python
+"""
+Adicionado docstring e shebang
+"""
+
+def funcao_soma(x, y):
+    return x+y
+
+def funcao_subtracao(x, y):
+    return x-y
+
+def funcao_multiplicacao(x, y):
+    return x*y
+
+def funcao_divisao(x, y):
+    return x/y
+
+def funcao_exp(x, y):
+    return x**y
+
+def funcao_raiz(x, y):
+    return x**(1/y)
+
+if __name__ == "__main__":
+
+
+    print("Hello World")
+    print("Matheus Torres")
+    print("Container executado com sucesso")
+    #Verifica se o Sonar informa melhoria.
+    IP = '192.168.0.12'
